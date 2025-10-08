01215031687692b987ce7cc96b26a32d67e7b891
waleedahmed-dot <46202@students.riphah.edu.pk>
2025-10-08 13:57:41 +0500

    Add files via upload

---
+++ my-ci-demo/sum.test.js
@@ -0,0 +1,10 @@
+// sum.test.js
+const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds negative numbers', () => {
+  expect(sum(-1, -1)).toBe(-2);
+});
